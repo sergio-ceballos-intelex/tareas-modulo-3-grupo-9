4ba4ed58d86a7e90339f5e4a11520a809757dbe4
Daniel-Cardenas <danielcar_777@hotmail.es>
2024-06-25 21:28:20 -0500

    Tercer commit, numeros del 2 al 2000 que empiezan por 2

--- Daniel-Cardenas-3196083047/Tarea3/Tarea3.js
+++ Daniel-Cardenas-3196083047/Tarea3/Tarea3.js
@@ -1,3 +1,6 @@
-for (let y = 1; y <= 2000; y++){
-    console.log(y, "\n");
+for (let y = 2; y <= 2000; y++){
+    let dos = y.toString();
+    if (dos.startsWith('2')){
+        console.log(dos)
+    };
 }
