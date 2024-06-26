b5c1f78fa7f01aefc73fe079f3b321a6ee743df5
AlejandroRodriguez <usuariolg80@gmail.com>
2024-06-26 13:29:03 -0500

    Se modifica conteo de for a 200 para aumentar array a 200

--- Alejandro-Rodriguez-3115276934/tarea-3/main.js
+++ Alejandro-Rodriguez-3115276934/tarea-3/main.js
@@ -1,5 +1,11 @@
-let conteo= [];
+/* let conteo= [];
 for(let i=1; i <= 50; i++){
+    conteo.push(i)
+}
+console.log(conteo) */
+
+let conteo= [];
+for(let i=1; i <= 200; i++){
     conteo.push(i)
 }
 console.log(conteo)
