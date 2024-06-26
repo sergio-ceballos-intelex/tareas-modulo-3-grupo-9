ff44c86ba1aac40ff5509c0f70c45c183be84df4
harri <harrybalanta@gmail.com>
2024-06-25 22:22:57 -0500

    se modifico programa que muestra los numeros del 1 al 2000 para que mostrara solo los que inician en 2

--- HARRINSON_BALANTA_3137735896/Tarea3/main.js
+++ HARRINSON_BALANTA_3137735896/Tarea3/main.js
@@ -1,9 +1,9 @@
-for(let num =1; num <=500; num ++){
 
-let ope = num;
-if (ope = 2, "<br>"){
-    
-console.log(num)
+for (let num = 2; num <= 2000; num++){
+    let ope = num.toString();
+    if (ope.startsWith('2')){
+        console.log(ope)
+    };
 }
-}
-// nota este programa lo intente pero no me funciono
+
+
