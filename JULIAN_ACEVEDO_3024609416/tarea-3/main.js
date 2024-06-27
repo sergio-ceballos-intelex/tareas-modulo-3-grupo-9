d6ca346d059a3e57bedf0a26f3158cdc48f9a5b9
Julian Acevedo <jlopezvet918@gmail.com>
2024-06-27 01:59:57 -0500

    script para mostrar en consola solo numeros que empiezan por 2

--- JULIAN_ACEVEDO_3024609416/tarea-3/main.js
+++ JULIAN_ACEVEDO_3024609416/tarea-3/main.js
@@ -1,4 +1,7 @@
-// Mostrar en consola el conteo de 1 a 50
+// Mostrar en consola el conteo de 1 a 2000
 for (let i = 1; i <= 2000; i++) {
-  console.log(i);
+  let numString = i.toString();
+  if (numString.startsWith("2")) {
+    console.log(numString);
+  }
 }
