c9fe63dacaead5fc4192d74f4e7cdc7920e6ab31
walter castro <walterc.100@gmail.com>
2024-06-27 20:50:03 -0500

    realizo tercer commit

--- walter-castro-3006265838/tarea-3/tarea-3.js
+++ walter-castro-3006265838/tarea-3/tarea-3.js
@@ -5,7 +5,15 @@
 // }
 
 //* Modificar el script para que ahora imprima en consola los numeros del 1 al 2000.
-for(let i=1; i<=2000;i++){
-    console.log('numero '+i)
-}
+// for(let i=1; i<=2000;i++){
+//     console.log('numero '+i)
+// }
 
+//*Modificar el script para que ahora imprima solo los numeros que empiezan por 2 (convertir a string el numero y verificar con starsWith)
+
+for(let i=1; i<=2000;i++){
+
+    if(i.toString().startsWith('2')){
+        console.log('numero '+i)
+    }   
+}
