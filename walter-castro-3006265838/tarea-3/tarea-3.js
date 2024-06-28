29d07a069477c5da7889b715baafa442724277fc
walter castro <walterc.100@gmail.com>
2024-06-27 20:44:38 -0500

    se realiza segundo commit

--- walter-castro-3006265838/tarea-3/tarea-3.js
+++ walter-castro-3006265838/tarea-3/tarea-3.js
@@ -1,5 +1,11 @@
 //*Crear un archivo .js y desarrollar un script que muestre en consola los numeros del 1 al 50.
 
-for(let i=1; i<=50;i++){
+// for(let i=1; i<=50;i++){
+//     console.log('numero '+i)
+// }
+
+//* Modificar el script para que ahora imprima en consola los numeros del 1 al 2000.
+for(let i=1; i<=2000;i++){
     console.log('numero '+i)
 }
+
