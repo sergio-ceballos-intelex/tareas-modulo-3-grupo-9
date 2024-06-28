1017354d73760471c5dc689de2f35da0a82d6dc9
carlossanchez <caredusanchez@hotmail.com>
2024-06-27 19:55:05 -0500

    uso start with

--- carlos-sanchez-312449/tarea-2/tarea-3/tarea3.js
+++ carlos-sanchez-312449/tarea-2/tarea-3/tarea3.js
@@ -1,3 +1,15 @@
-for (let conteo = 1; conteo <= 2000 ; conteo ++){
-    console.log(conteo);
+// for (let conteo = 1; conteo <= 2000 ; conteo ++){
+//     console.log(conteo);
+// }
+
+//let excluyente = [123, 256,202, 45];
+
+let number = 2000;
+for(let cont = 1; cont <= number ; cont++){
+    let numstring = cont.toString();
+
+    if (numstring.startsWith(2)){
+        console.log(numstring)
+    } 
 }
+    
