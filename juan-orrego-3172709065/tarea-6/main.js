b9510aae5a917396b8f702f5e2906224bf808316
Juan_Diego <juan3312orrego@gmail.com>
2024-07-04 20:09:14 -0500

    2. Encontrar el producto con el precio más bajo.

--- juan-orrego-3172709065/tarea-6/main.js
+++ juan-orrego-3172709065/tarea-6/main.js
@@ -1,4 +1,7 @@
 // 1. multiplicar cada número por 2 y almacenar el resultado en un nuevo array.
+const mensaje = (num) => console.log(`---PUNTO ${num}---`);
+
+mensaje(1);
 let numeros = [1, 2, 3, 4, 5];
 let numPorDos = []
 
@@ -7,3 +10,24 @@ for (let i = 1; i <= numeros.length; i++) {
 }
 
 console.log(numPorDos);
+
+// 2. Encontrar el producto con el precio más bajo. imprime el nombre y el precio de ese producto.
+mensaje(2);
+let productos = [
+    { nombre: "Camisa", precio: 20 },
+    { nombre: "Pantalón", precio: 30 },
+    { nombre: "Zapatos", precio: 50 },
+  ];
+
+let precioM = productos[0].precio;
+let nombreProM = productos[0].nombre;
+
+for (let i = 0; i < productos.length; i++) {
+    const element = productos[i];
+    if(element.precio < precioM){
+        precioM = element.precio;
+        nombreProM = element.nombre;
+    }
+}
+
+console.log(`${nombreProM}, es el producto con el precio más bajo, tiene un precio de: ${precioM}`);
