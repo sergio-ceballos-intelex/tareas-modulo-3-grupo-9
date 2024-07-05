ddc6ec0747b9fed93da151cc9a76066de7e9599f
harri <harrybalanta@gmail.com>
2024-07-04 21:58:32 -0500

    se realiza el ejercicio 3 detallado  paso de las expresiones

--- HARRINSON_BALANTA_3137735896/Tarea5/mainlog.js
+++ HARRINSON_BALANTA_3137735896/Tarea5/mainlog.js
@@ -1,4 +1,32 @@
-// Lógica proposicional
+// Lógica proposicional punto 1
 console.log (true || true || false && true || false && true && true || false)
+console.log (true || false && true || false && true && true || false)
+console.log (true  && true || false && true && true || false)
+console.log (true || false && true && true || false)
+console.log (true && true && true || false)
+console.log (true && true || false)
+console.log (true || false)
+console.log (true)
+// Lógica proposicional punto 2
 console.log(true && (false || false && (true && (true || false && true))) || false && (true || false))
+console.log(true && (false || false && (true && (true || false && true))) || false && true )
+console.log(true && (false || false && (true && (true || false))) || false && true )
+console.log(true && (false || false && (true && true)) || false && true )
+console.log(true && (false || false &&  true) || false && true )
+console.log(true && (false || false ) || false && true )
+console.log(true && false || false  || false)
+console.log( false || false  || false)
+console.log( false  || false)
+console.log(false)
+
+// Lógica proposicional punto 3
 console.log (!(!true && (false || false && !!!true || (true && !false))) || false && true)
+console.log (!(!true && (false || false && !!!true || true)) || false && true)
+console.log (!(!true && (false || false && false || true)) || false && true)
+console.log (!(!true && (false || true)) || false && true)
+console.log (!(!true && true) || false && true)
+console.log (!(false && true) || false && true)
+console.log (!false || false && true)
+console.log (true || false && true)
+console.log (true || false)
+console.log (true)
