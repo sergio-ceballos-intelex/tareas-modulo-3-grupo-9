23d5a3dd9af3e3e37cc20dd136ed80f0acb6f069
jhon caldon <brandiny92@gmail.com>
2024-07-05 11:45:06 -0500

    subo la tercer activad logica proporcional

--- jhon_caldon_3107994758/tarea5/solucion.js
+++ jhon_caldon_3107994758/tarea5/solucion.js
@@ -1,36 +1,22 @@
-// Registro de un estudiante 
-const name=prompt("Digita tu nombre ");
-const edad=Number(prompt("Digita tu edad"));
-const notas=Number(prompt("Digita el promedio de tus notas entre 0 a 10"));
-const regular = confirm("¿Eres un estudiante regular?");
-const materias= Number(prompt("Digita las materias aprobadas"))
-const beca= confirm("¿Eres becado?")
-  document.write(
-    "Nombre del estudiante:"+ name+ "<br>"+
-    "Edad:"+ edad+"<br>"+
-    "Promedio de Notas: " + notas+"<br>"+
-    "Estudiante Regular: " +( regular? "si":"no")+"<br>"+
-    "Materias Aprobadas:"+ materias+ "<br>"+
-    "Es becado: "+ (beca? "si":"no")+"<br>"
-  )
-if (edad < 18)
-    {
-        document.write("Eres menor de edad no podemos registrar");
-    }
-    else if (notas<= 5)
-        {
-            document.write("No cumples con el promedio minimo de notas requerido");
-        }
-    else if (regular == false)
-        {
-            document.write("Debes primero registrarte como regular");
-        
-        }
-    else if (beca===false)
-        {
-            document.write("Puedes solicitar una beca ya que cumples con los requisitos");
-        }
-    else if (notas>=6 && regular && beca){
-            document.write("Felicidades cumples con todos los requisitos felicidades")
-        }
-   
+// logica proposicional
+true || true || false && true || false && true && true || false     
+true || true || false || false || false     
+true || true || false || false    
+true || false    
+true  
+
+___________________________________________________________________________
+
+true && (false || false && (true && (true || false && true))) || false && (true || false)    
+true && (false || false && (true && true)) || false && true    
+true && (false || false && true) || false  
+true && false || false  
+false  
+
+___________________________________________________________________________
+
+!(!true && (false || false && !!!true || (true && !false))) || false && true  
+!(!true && (false || false && !!!true || true)) || false  
+!(!true && true) || false  
+!false || false  
+true   
