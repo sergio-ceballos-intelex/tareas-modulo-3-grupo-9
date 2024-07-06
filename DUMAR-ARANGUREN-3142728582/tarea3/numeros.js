fe06ca285b75cfc2c2b8ea1bc668bb632c3953ab
Dmar40 <dumararanguren@gmail.com>
2024-07-05 23:22:25 -0500

    actualizo tarea3

--- DUMAR-ARANGUREN-3142728582/tarea3/numeros.js
+++ DUMAR-ARANGUREN-3142728582/tarea3/numeros.js
@@ -1,3 +1,12 @@
-for (let i = 1; i <= 2000; i++) {
-    console.log(i);
+// for (let i = 1; i <= 2000; i++) {
+//     console.log(i);
+// }
+
+for (let cont = 1; cont <= 2000; cont++) {
+    const convert = cont.toString()
+    if (convert.startsWith("2")) {
+        console.log(convert)
+    }
+    
 }
+   
