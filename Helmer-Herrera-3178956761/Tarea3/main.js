85ea1bb3737d5ee83e485ad4f97621573b59a530
Helmer Herrera <helmer.herrera@gmail.com>
2024-07-07 20:07:08 -0500

    Se cambia el scritp de la tabla inicial cambiando el limite con los numeros ahora del 1 al 2000

--- Helmer-Herrera-3178956761/Tarea3/main.js
+++ Helmer-Herrera-3178956761/Tarea3/main.js
@@ -1,3 +1,8 @@
 for (let num = 1; num <=50; num ++) {
         console.log(num);   
     }
+// Se modifica el limite del script a una tabla con limite de numeros hasta 2000 
+for (let num = 1; num <=2000; num ++) {
+    console.log(num);   
+}    
+
