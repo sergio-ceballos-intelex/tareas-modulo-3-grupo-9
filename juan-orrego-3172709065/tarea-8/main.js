ce0c40dc8877a34c069bab56bd42e4f30e0bc7fd
Juan_Diego <juan3312orrego@gmail.com>
2024-07-11 11:47:03 -0500

    2. Callbacks para Filtrar Elementos de un Arreglo.

--- juan-orrego-3172709065/tarea-8/main.js
+++ juan-orrego-3172709065/tarea-8/main.js
@@ -10,3 +10,16 @@ function sumarArreglo(numeros, callback) {
 sumarArreglo(numeros, function (resultado) {
   console.log(resultado); // Debería imprimir 15
 });
+
+// Punto 2. Callbacks para Filtrar Elementos de un Arreglo:
+// Crea una función que tome un arreglo y un callback de filtro, y devuelva un nuevo arreglo con los elementos que pasan el filtro. Ejercicio:
+
+function filtrarArreglo(arreglo, callbackFiltro) {
+  return arreglo.filter(callbackFiltro);
+}
+
+const numerosDos = [1, 2, 3, 4, 5];
+const numerosPares = filtrarArreglo(numerosDos, function (numero) {
+  return numero % 2 === 0;
+});
+console.log(numerosPares); // Debería imprimir [2, 4]
