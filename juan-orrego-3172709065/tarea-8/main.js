92d9062fe72e023e2790a40910a87ef71289b103
Juan_Diego <juan3312orrego@gmail.com>
2024-07-11 20:10:37 -0500

    3. Callbacks: Filtrar Números Pares.

--- juan-orrego-3172709065/tarea-8/main.js
+++ juan-orrego-3172709065/tarea-8/main.js
@@ -23,3 +23,31 @@ const numerosPares = filtrarArreglo(numerosDos, function (numero) {
   return numero % 2 === 0;
 });
 console.log(numerosPares); // Debería imprimir [2, 4]
+
+// Punto 3. Callbacks: Filtrar Números Pares
+/* Descripción: Creea una función en JavaScript que filtre los números pares de un array y utilicen un callback para manejar el resultado.
+
+Pasos a seguir:
+
+Crea una función llamada filtrarPares que acepte dos parámetros: un array de números y un callback.
+Dentro de la función filtrarPares, utiliza el método filter para filtrar los números pares del array.
+Llama al callback pasado como parámetro con el array de números pares como argumento.
+Implementa un callback llamado mostrarPares que acepte un parámetro numerosPares. Este callback debe imprimir los números pares en la consola.*/
+
+
+// Función para filtrar números pares y llamar a un callback con el resultado
+function filtrarPares(numeros, callback) {
+  const numPares = numeros.filter((num) => num % 2 == 0)
+  callback(numPares) 
+}
+
+// Callback para mostrar los números pares en la consola
+function mostrarPares(numerosPares) {
+  console.log(numerosPares)
+}
+
+// Array de números de ejemplo
+const numerosTres = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Llamar a la función filtrarPares y pasar el callback mostrarPares
+filtrarPares(numerosTres, mostrarPares);
