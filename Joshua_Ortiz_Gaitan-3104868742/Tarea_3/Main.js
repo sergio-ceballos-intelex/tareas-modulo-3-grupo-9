9bf3afc4cd8edafec3c2d317890bae41a6643173
Joshua <jortiz336@misena.edu.co>
2024-07-15 23:30:10 -0700

    Cree un script que los numeros del 1 al 2000. pero con el toString lo pasa a tipo de dato string para que despues el .startsWith lo pueda leer ya que solo admite texto y asisolo escoger los numeros que empiezan con el valor 2

--- Joshua_Ortiz_Gaitan-3104868742/Tarea_3/Main.js
+++ Joshua_Ortiz_Gaitan-3104868742/Tarea_3/Main.js
@@ -1,5 +1,20 @@
 
-for (let Contador = 0; Contador <= 50; Contador++){
-    console.log(Contador);
+// for (let Contador = 0; Contador <= 50; Contador++){
+//     console.log(Contador);
+// }
+
+
+
+
+// for (let Contador = 0; Contador <= 2000; Contador++){
+//     if (Contador % 2 === 0){console.log(Contador);}
+    
+// }
+
+
+
+for (let Inicia = 0; Inicia <= 2000; Inicia++){
+    if (Inicia.toString().startsWith('2')){console.log(Inicia);}
+    
 }
 
